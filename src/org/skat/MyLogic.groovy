d122e3ef1cd9f92adcab7b6ccc138537ca21c212
mllk99 <34234384+mllk99@users.noreply.github.com>
2017-12-06 15:43:53 +0100

    Update MyLogic.groovy

--- src/org/skat/MyLogic.groovy
+++ src/org/skat/MyLogic.groovy
@@ -4,4 +4,12 @@ class MyLogic {
     def helloFromMyMagic() {
         return "Hello from MyMagic"
     }
+    
+    //def echoBranch(...) {
+        
+    //}
+    //def safeBranch = env.GIT_BRANCH //?: '' 
+        
+    //def echoBranch(...)
 }
+
