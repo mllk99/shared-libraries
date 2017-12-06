b1924829fd383093fff6fa8928154fe46a1506bd
mllk99 <34234384+mllk99@users.noreply.github.com>
2017-12-06 14:42:33 +0100

    Update mymaven.groovy

--- vars/mymaven.groovy
+++ vars/mymaven.groovy
@@ -1,9 +1,10 @@
 //import org.skat.MyLogic
 
 def call() {
-
+    
+    echo "Running mvn clean package"
     sh 'docker container run -i --rm --name my-maven-project -v "$PWD":/usr/src/mymaven -w /usr/src/mymaven maven:3-jdk-8 mvn clean package'
     archiveArtifacts 'target/gildedrose-*.jar'
     //    def myLogic = new MyLogic()
-//    echo "${myLogic.helloFromMyMagic()}"
+    //    echo "${myLogic.helloFromMyMagic()}"
 }
