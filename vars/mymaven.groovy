1858e4cb75ab92546ccf83793015c0b7b4f2fd91
mllk99 <34234384+mllk99@users.noreply.github.com>
2017-12-19 10:16:25 +0100

    Update mymaven.groovy
    test

--- vars/mymaven.groovy
+++ vars/mymaven.groovy
@@ -2,6 +2,7 @@
 
 def call() {
     def projectname = env.BUILD_TAG
+    echo "Project name"
     echo "${projectname}" 
     echo "Running mvn clean package"
     sh 'docker container run -i --rm --name "${projectname}" -v "$PWD":/usr/src/mymaven -w /usr/src/mymaven maven:3-jdk-8 mvn clean package'
