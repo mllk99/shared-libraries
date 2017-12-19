1c81a89482bc203964cf62601f86bc0c5edee1de
mllk99 <34234384+mllk99@users.noreply.github.com>
2017-12-19 11:00:06 +0100

    Update mymaven.groovy

--- vars/mymaven.groovy
+++ vars/mymaven.groovy
@@ -5,7 +5,7 @@ def call() {
     echo "Project name"
     echo "${projectname}" 
     echo "Running mvn clean package"
-    sh 'docker container run -i --rm --name "${projectname}" -v "$PWD":/usr/src/mymaven -w /usr/src/mymaven maven:3-jdk-8 mvn clean package'
+    //sh 'docker container run -i --rm --name "${projectname}" -v "$PWD":/usr/src/mymaven -w /usr/src/mymaven maven:3-jdk-8 mvn clean package'
     archiveArtifacts 'target/gildedrose-*.jar'
     //    def myLogic = new MyLogic()
     //    echo "${myLogic.helloFromMyMagic()}"
